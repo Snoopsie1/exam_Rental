c1b140b98c5069ad8d8e6db4779744b21f1f7f6a
Snoopsie1 <oliver.rasoli@gmail.com>
2022-06-15 21:35:39 +0200

    added US3 Rest

--- src/main/java/rest/AdminResource.java
+++ src/main/java/rest/AdminResource.java
@@ -4,7 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dtos.HouseDTO;
 import dtos.RentalDTO;
+import dtos.TenantDTO;
 import entities.Rental;
+import entities.Tenant;
 import repository.AdminRepo;
 import repository.UserRepo;
 import utils.EMF_Creator;
@@ -25,7 +27,7 @@ public class AdminResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
 
-    private static final AdminRepo adminRepo = AdminRepo.getAdminRepo(EMF);
+    private static final AdminRepo adminREPO = AdminRepo.getAdminRepo(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     @GET
@@ -35,5 +37,18 @@ public class AdminResource {
     }
 
 
+    //TODO: US-3 As an admin, I would like to see all tenants currently living in a particular house
+    @GET
+    @Path("/house/{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getAllTenantsByHouseId(@PathParam("id") int id) throws EntityNotFoundException{
+        List<Tenant> tenantList = adminREPO.getTenantsInSpecificHouse(id);
+        List<TenantDTO> tenantDTOS = new ArrayList<>();
+        for (Tenant tenant : tenantList) {
+            tenantDTOS.add(new TenantDTO(tenant));
+        }
+
+        return Response.ok().entity(GSON.toJson(tenantDTOS)).build();
+    }
 
 }
